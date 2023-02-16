2f7bae459dcbf004d1a910c992caf25d56cb0dd0
issaevis <evis.topollaj@gmail.com>
2023-02-16 08:31:59 -0800

    Added 11

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Printing to the standard error
+ * Return: Will return 1 if everything went well
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(STDERR_FILENO, str, sizeof(str) - 1);
+
+	return (1);
+}
