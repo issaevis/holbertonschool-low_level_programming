e429ebe993966e3a0c7745ae3fa5b279da5d3c0b
issaevis <evis.topollaj@gmail.com>
2023-02-16 04:20:50 -0800

    Added 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ *This program outputs a predetermined string using the puts function.
+ *Author: issaevis 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	char str[] = "\"Programming is like building a multilingual puzzle\0";
+	
+	puts(str);
+	
+	return 0;
+}
