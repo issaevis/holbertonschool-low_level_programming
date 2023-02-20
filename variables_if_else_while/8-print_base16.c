ab01e481fe1b851d5eadae9fdde1d2eaf92076e3
issaevis <evis.topollaj@gmail.com>
2023-02-20 04:18:17 -0800

    Fixed 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,13 +10,13 @@
  * Return: Will return 0 if everything went well
  */
 
-int main (void)
+int main(void)
 {
 	int i;
-	
-	for (i=48 ; i < 58 ; i++)
+
+	for (i = 48 ; i < 58 ; i++)
 		putchar(i);
-	for (i=97 ; i < 103 ; i++)
+	for (i = 97 ; i < 103 ; i++)
 		putchar(i);
 	putchar('\n');
 
