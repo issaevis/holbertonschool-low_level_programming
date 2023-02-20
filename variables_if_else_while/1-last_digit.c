4620eef676de72cef365f62b6bd94d01a632c497
issaevis <evis.topollaj@gmail.com>
2023-02-20 02:08:41 -0800

    Added 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+/*
+ *This program prints a number
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints number, tells if its positive or negative
+ * Return: Will return 0 if everything went well
+ */
+
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	if (lastDigit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	else if (lastDigit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	else if (lastDigit < 6 && lastDigit != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	return (0);
+}
