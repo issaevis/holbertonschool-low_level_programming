f97c75eff5d2dc4c5e76acf0d5b6ac8964612414
issaevis <evis.topollaj@gmail.com>
2023-02-20 03:40:35 -0800

    Added 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/*
+ *This program prints 0123456789
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints a chars and a new line 
+ * Return: Will return 0 if everything went well
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	printf("\n");
+return (0);
+}
