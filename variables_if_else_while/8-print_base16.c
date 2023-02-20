a321f7bf39af284a8e710fa3ab3bb41df57105ac
issaevis <evis.topollaj@gmail.com>
2023-02-20 04:16:13 -0800

    Added 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/*
+ *This program prints all possible HEX numbers
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints chars and a new line after
+ * Return: Will return 0 if everything went well
+ */
+
+int main (void)
+{
+	int i;
+	
+	for (i=48 ; i < 58 ; i++)
+		putchar(i);
+	for (i=97 ; i < 103 ; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
