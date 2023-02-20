6dbd8ba7fef4a32a74c1927f9176e8c922bb2930
issaevis <evis.topollaj@gmail.com>
2023-02-20 03:33:27 -0800

    Added 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ *This program prints the alphabet and \n
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints a char and a new line excluding q and e
+ * Return: Will return 0 if everything went well
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
+
