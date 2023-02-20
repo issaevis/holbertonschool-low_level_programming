47fd9a00e26bc81f0f0dea1452bf1dc00e61b63e
issaevis <evis.topollaj@gmail.com>
2023-02-20 02:23:35 -0800

    Added 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/*
+ *This program prints the alphabet and \n
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints a char and a new line
+ * Return: Will return 0 if everything went well
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i<=122; i++)
+		printf("%c\n", i);
+	return (0);
+}
