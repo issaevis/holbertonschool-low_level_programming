58c1ca850b16c8838b472e2d9b41f02b686f3b4a
issaevis <evis.topollaj@gmail.com>
2023-02-21 01:01:33 -0800

    Added 0 and README

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+ *This program prints _putchar
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints a string and a new line after
+ * Return: Will return 0 if everything went well
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	printf("%s\n", str);
+	return (0);
+}
+
