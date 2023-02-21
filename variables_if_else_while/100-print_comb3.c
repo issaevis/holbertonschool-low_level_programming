dd0138f766d3827f8ee0f5810cf808df742130db
issaevis <evis.topollaj@gmail.com>
2023-02-21 10:38:03 -0800

    Fixed 100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+/*
+ *This program prints all double digit numbers
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints numbers, separated by a comma followed by space
+ * Return: Will return 0 if everything went well
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0' ; i <= '8' ; i++)
+	{
+		for (j = i + 1  ; j <= '9' ; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
