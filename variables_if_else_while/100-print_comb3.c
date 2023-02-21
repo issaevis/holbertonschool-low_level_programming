1a17c7fdf1723f3fd3b84d0f15764de38031b1b0
issaevis <evis.topollaj@gmail.com>
2023-02-21 10:45:00 -0800

    Fixed 10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -21,14 +21,15 @@ int main(void)
 			putchar(i);
 			putchar(j);
 
-			if (i != '8' && j != '9')
+			if (i == '8' && j == '9')
+				putchar('\n');
+			else	
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
 
 	return (0);
 }
