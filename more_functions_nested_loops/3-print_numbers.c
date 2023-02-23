16e7a6c79b10e29fa87b9ca3c126948fd5f02366
issaevis <evis.topollaj@gmail.com>
2023-02-23 11:30:30 -0800

    Added 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * print_numbers- prints numbers from 0 to 9 with a \n
+ *
+ * Return: returns nothing
+ */
+
+#include "main.h"
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar('0' + i);
+	}
+
+	_putchar('\n');
+}
