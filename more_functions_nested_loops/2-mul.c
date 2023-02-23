afb6d6304fe75ab7a797e5305e1f7279f48e1167
issaevis <evis.topollaj@gmail.com>
2023-02-23 11:15:28 -0800

    Added 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul- multiplies two numbers
+ * @a: the first number we get from the main
+ * @b: the second number we get from the main
+ *
+ *
+ * Return: returns the sum of the numbers
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
