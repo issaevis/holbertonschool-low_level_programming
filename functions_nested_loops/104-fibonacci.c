c0cd80ae5ff641d9195b51a81da0e737efc29bdd
issaevis <evis.topollaj@gmail.com>
2023-02-24 04:12:05 -0800

    Added 16

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/**
+ * main- prints the first 100 numbers of the Fibonacci sequence
+ *
+ * Return: returns 0 if everything went well
+ */
+
+int main(void)
+{
+	int i, num1 = 0, num2 = 1, nextNum;
+	
+	for (i = 1; i < 100; i++) 
+	{
+		if (i <= 1)
+		{
+			nextNum = i;
+		}
+		else
+		{
+			nextNum = num1 + num2;
+			num1 = num2;
+			num2 = nextNum;
+		}
+		printf("%d ", nextNum);
+	}
+	printf("\n");
+	return (0);
+}
+
+
