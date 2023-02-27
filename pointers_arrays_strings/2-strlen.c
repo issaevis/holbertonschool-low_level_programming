1ba0eae508eca4d1290aa2d68c97206bb26d9d25
issaevis <evis.topollaj@gmail.com>
2023-02-27 01:34:59 -0800

    Added 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+/**
+ * _strlen- returns the length of a string
+ * @s: the address of a string
+ *
+ * Return: returns nothing to the main
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; i < 1000 ; i++)
+	{
+		if (s[i] == '\0')
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (i);
+}
