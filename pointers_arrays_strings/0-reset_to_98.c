5026395d30e3c2eb3998433e04939ee1916ad2a2
issaevis <evis.topollaj@gmail.com>
2023-02-27 01:07:53 -0800

    Added 0 and README

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/**
+ * print_square- prints a square
+ * @size: how big the square is
+ *
+ *
+ * Return: returns nothing to the main
+ */
+
+#include <stdio.h>
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
