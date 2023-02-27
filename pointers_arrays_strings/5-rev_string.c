4140ba9ad09e41e8ebc120b7d16db5a22ed0c741
issaevis <evis.topollaj@gmail.com>
2023-02-27 04:39:28 -0800

    Added 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+/**
+ * print_rev- prints a string in reverse order
+ * @s: the address of a string
+ *
+ * Return: returns nothing to the main
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+void rev_string(char *s)
+{
+	int i = 0, j, len;
+	char tmp;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		len = i;
+	}
+
+	j = len - 1;
+	i = 0;
+
+	while (j >= i)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+
+		i++;
+		j--;
+	}
+}
