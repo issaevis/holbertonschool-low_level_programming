23c16d494ee66f93d621aac4fc39cb17d5d3cf1c
issaevis <evis.topollaj@gmail.com>
2023-03-01 09:12:18 -0800

    Fixed 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,16 @@
 /**
  * reverse_array- reverses the contents of an array
  * @n: the number of elements of the array
- *
+ * @a: start of the array
  *
  * Return: returns nothing to the main
  */
 
 #include "main.h"
 
-#define MAX_SIZE 100
-
 void reverse_array(int *a, int n)
 {
-	int i, arr[MAX_SIZE], j = n - 1;
+	int i, arr[100], j = n - 1;
 
 	for (i = 0 ; i < n ; i++)
 	{
