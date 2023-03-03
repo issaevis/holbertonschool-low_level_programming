c4890ee80052af584b1248b5588ea204ef495b92
issaevis <evis.topollaj@gmail.com>
2023-03-03 06:45:30 -0800

    Added 8 from the third project

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+/**
+ * print_chessboard- prints a chessboard
+ * @a: the length of the first index of the 2D array
+ *
+ *
+ * Return: returns nothing to the main
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - i - 1));
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
