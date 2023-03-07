4a615aaf650e2041cad842d20ac25ec524d624d9
issaevis <evis.topollaj@gmail.com>
2023-03-07 02:23:50 -0800

    Added 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,7 +9,29 @@
 
 int _sqrt_recursion(int n)
 {
-	if ((n \ n) == 1  )
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (find_sqrt(n, 2));
+}
+
+/**
+ * find_sqrt- facilitates the use of the previous function
+ * @n: the number we get from the previous function
+ * @i: the number we are returning to _sqrt_recursion
+ *
+ * Return: returns an int to _sqrt_recursion
+ */
+
+
+int find_sqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i <= n)
+		return (find_sqrt(n, i + 1));
+	else
 		return (-1);
 
 }
