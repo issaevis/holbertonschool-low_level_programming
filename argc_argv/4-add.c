57dd95d3bbf52fef6ba7f752dde7859b723d33ec
issaevis <evis.topollaj@gmail.com>
2023-03-08 04:40:45 -0800

    Fixed 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,9 +2,10 @@
  *This program adds multiple numbers
  *Author:issaevis
  */
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - Prints the sum of two or more numbers
  * @argc: The count of the arguments
@@ -21,7 +22,13 @@ int main(int argc, char *argv[])
 	{
 		if (*argv[i] >= '0' && *argv[i] <= '9')
 		{
-			sum = sum + atoi(argv[i]);
+			if (is_num(argv[i]) == 1)
+				sum = sum + atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				exit(-1);
+			}
 		}
 		else
 		{
@@ -31,4 +38,26 @@ int main(int argc, char *argv[])
 	}
 	printf("%d\n", sum);
 	return (0);
+}
+
+/**
+ * is_num - Prints the sum of two or more numbers
+ * @s: The string we get from main
+ *
+ * Return: Will return 1 if its a number and -1 if its not
+ */
+
+
+int is_num(char *s)
+{
+	int i;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			continue;
+		else
+			return (-1);
+	}
+	return (1);
 }
---
+++ argc_argv/main.h
@@ -0,0 +1,1 @@
+int is_num(char *s);
