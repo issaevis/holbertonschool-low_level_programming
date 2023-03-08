3de36188a1fb23a0377f87cea0ab2214bf5af0cb
issaevis <evis.topollaj@gmail.com>
2023-03-08 01:34:11 -0800

    Added 0 and the README

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+/*
+ *This program prints its name
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the filename
+ * Return: Will return 0 if everything went well
+ */
+
+int main (int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
