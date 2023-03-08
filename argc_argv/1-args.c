ff524b8d28b22cd7b6ec86462822293c44cac704
issaevis <evis.topollaj@gmail.com>
2023-03-08 01:49:37 -0800

    Added 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+/*
+ *This program prints the number of arguments
+ *Author:issaevis
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments
+ * @argc: The count of the arguments
+ * @argv: An array with the arguments
+ *
+ * Return: Will return 0 if everything went well
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc);
+	return (0);
+}
