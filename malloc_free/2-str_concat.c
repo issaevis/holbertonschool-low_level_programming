5615691ffcd3a33d0b941d55804d93c246644161
issaevis <evis.topollaj@gmail.com>
2023-03-13 09:31:49 -0700

    Added 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+/**
+ * str_concat- creates a string with the content of two other strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: returns the first address of the new string
+ */
+
+#include <stdlib.h>
+#include "main.h"
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, index, so1, so2;
+	char *s;
+
+	while (s1[i] != '\0')
+		i++;
+	so1 = i;
+	i = 0;
+
+	while (s2[i] != '\0')
+		i++;
+	so2 = i;
+
+	s = malloc(sizeof(char) * (so1 + so2 + 1));
+	if (s == NULL)
+		return (NULL);
+
+
+	for (i = 0 ; i < so1 ; i++)
+	{
+		s[i] = s1[i];
+	}
+	index = i;
+
+	for (i = 0 ; i < so2 ; i++)
+	{
+		s[index + i] = s2[i];
+	}
+	s[index + i] = '\0';
+
+	return (s);
+}
