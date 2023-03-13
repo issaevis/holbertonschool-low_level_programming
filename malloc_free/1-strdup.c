dd9ea17b534ef5284a2a98170c311f7c5e5c338c
issaevis <evis.topollaj@gmail.com>
2023-03-13 08:15:56 -0700

    Fixed 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,11 +14,15 @@ char *_strdup(char *str)
 	int i, size;
 	char *s;
 
+	if (str == NULL)
+		return (NULL);
+
 	i = 0;
 
 	while (str[i] != '\0')
 		i++;
 	size = i;
+
 	if (size > 0)
 	{
 		s = malloc(sizeof(char) * size);
