57676872212fbb415cffc7d7403a73fb8cf9c121
issaevis <evis.topollaj@gmail.com>
2023-03-13 08:03:19 -0700

    Added 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+/**
+ * _strdup- creates a copy of a string with malloc
+ * @str: the string we need to copy
+ *
+ *
+ * Return: returns the first address of the new string
+ */
+
+#include <stdlib.h>
+#include "main.h"
+
+char *_strdup(char *str)
+{
+	int i, size;
+	char *s;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	size = i;
+	if (size > 0)
+	{
+		s = malloc(sizeof(char) * size);
+		if (s == NULL)
+			return (NULL);
+
+		for (i = 0; i < size; i++)
+		{
+			s[i] = str[i];
+		}
+		s[i] = '\0';
+	}
+	else
+		return (NULL);
+
+	return (s);
+}
