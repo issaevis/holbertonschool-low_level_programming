21bf778726ffed14890e3758f55139a436bc682c
issaevis <evis.topollaj@gmail.com>
2023-03-15 10:30:37 -0700

    Added 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,19 @@
+/*
+ *This program prints the filename and \n
+ *Author:issaevis
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file 
+ *
+ * Return: Will return 0 if everything went well
+ */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
