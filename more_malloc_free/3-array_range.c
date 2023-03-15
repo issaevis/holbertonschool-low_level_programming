8500f48082480824ed8f9a4378a9b10df2618935
issaevis <evis.topollaj@gmail.com>
2023-03-15 08:45:18 -0700

    Added 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+/**
+ * array_range- creates an array and puts the numbers in order
+ * @min: the min number
+ * @max: the max number
+ *
+ * Return: returns the array
+ */
+
+#include <stdlib.h>
+#include "main.h"
+
+int *array_range(int min, int max)
+{
+	int *arr, i;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * (max - min));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (max - min); i++)
+	{
+		arr[i] = min + i;
+
+	}
+	return (arr);
+}
