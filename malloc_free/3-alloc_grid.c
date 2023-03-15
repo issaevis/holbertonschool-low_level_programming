e92c2c09a3fcbc3502e182336bb4d228c2c91cbf
issaevis <evis.topollaj@gmail.com>
2023-03-15 02:20:38 -0700

    Fixed 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -20,8 +20,10 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *) malloc(height * sizeof(int));
 
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
-
+	}
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
