76a4fcb4b6b5e58f8ec484085e593ed77b1c92fe
issaevis <evis.topollaj@gmail.com>
2023-03-15 09:05:33 -0700

    Fixed 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,14 +16,16 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min) + 1);
+	arr = malloc(sizeof(int) * (max - min) + sizeof(int));
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < (max - min); i++)
+	if (min == max)
+		arr[0] = max;
+	else
 	{
-		arr[i] = min + i;
-
+		for (i = 0; i < (max - min); i++)
+			arr[i] = min + i;
 	}
 	return (arr);
 }
