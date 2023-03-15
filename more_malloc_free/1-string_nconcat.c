88a12cb04e56a996b16e85a7507d9b78729ca84c
issaevis <evis.topollaj@gmail.com>
2023-03-15 05:43:21 -0700

    Fixed 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = 0;
 	nn = n;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i] != '\0')
 		i++;
 	so1 = i;
