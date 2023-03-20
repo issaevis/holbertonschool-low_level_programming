005678b7b7f1b15ba7836e284e5fa79c7932935e
issaevis <evis.topollaj@gmail.com>
2023-03-20 02:55:35 -0700

    Final fix of exercise 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,9 +10,17 @@
  */
 
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		printf("Ok\n");
+		exit(-1);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
