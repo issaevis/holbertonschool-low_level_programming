5e4131c0fc900cb113409a0ac90e7c476cdb16c8
issaevis <evis.topollaj@gmail.com>
2023-03-20 03:55:17 -0700

    Removed an if statement from exercise 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -23,10 +23,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	if (d->age == 0)
-		printf("Age: (nill)\n");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nill)\n");
