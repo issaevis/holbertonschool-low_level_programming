ae6c77490fbd2152585c2db352365543e4f610ca
issaevis <evis.topollaj@gmail.com>
2023-03-20 02:13:40 -0700

    Added a README and exercise 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a struct called dog
+ * @name: A string as a name
+ * @age: An int as age
+ * @owner: A string as the owner
+ *
+ * Description: This struct defines a new datatype with two strings and one int
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endif
