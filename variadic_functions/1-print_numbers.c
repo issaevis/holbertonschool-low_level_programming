3a644b3599ebd04f87434a977ab4fa7687028e94
issaevis <evis.topollaj@gmail.com>
2023-03-23 08:13:12 -0700

    Added exercise 1 and extra files for it

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+/**
+ * print_numbers- Prints all the ints given to it with the separators
+ *
+ * @separator: The separators between the numbers
+ * @n: the count of numbers
+ *
+ * Return: returns nothing to the main
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i;
+
+	if (separator == NULL)
+		exit(-1);
+
+	va_start(ap, n);
+	for (i = n; i > 0 ; --i)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
