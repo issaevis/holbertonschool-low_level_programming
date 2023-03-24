9c476f1b0b938925a4f15ddc957f4d68f692b023
issaevis <evis.topollaj@gmail.com>
2023-03-24 08:55:28 -0700

    Added exercise 2 and extra files

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,3 +1,5 @@
+#define MAX 100
+
 /**
  * print_strings- Prints all strings given to it with the separators
  *
@@ -10,18 +12,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
 
+	char placeholder[MAX];
+
 	va_start(ap, n);
 
 	for (i = n; i > 0; --i)
 	{
-		printf("%s", va_arg(ap, char*));
-		if (i != 1)
+		char *str = va_arg(ap, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+		{
+			strcpy(placeholder, str);
+			printf("%s", placeholder);
+		}
+
+		if (i != 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	va_end(ap);
