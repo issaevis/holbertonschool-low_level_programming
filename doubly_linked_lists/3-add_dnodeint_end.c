c698df9fd6f7165dd32aefc2f966045f393ca828
issaevis <evis.topollaj@gmail.com>
2023-04-05 02:47:04 -0700

    Fixed a minor bug when the list is empty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = new;
+		new->prev = NULL;
+		new->next = NULL;
+	}
 	else
 	{
 		tmp = *head;
