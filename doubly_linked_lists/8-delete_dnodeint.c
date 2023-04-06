7a3a6dae093e73e7c082767056694fffd2a6c87b
issaevis <evis.topollaj@gmail.com>
2023-04-06 08:07:13 -0700

    Fixed 8 and added the extra files it needs to run

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,17 +1,47 @@
-#include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
 #include <stdio.h>
-
+#include "lists.h"
 /**
- * sum_dlistint- adds all the ints in the node
+ * delete_dnodeint_at_index - Deletes the node at the given index of a
+ * doubly linked list.
+ * @head: Pointer to the head node of the list.
+ * @index: Index of the node to delete.
  *
- * @h: the head of the linked list
- *
- * Return: returns the sum or 0 if the list is empty
+ * Return: 1 if the node was deleted successfully, -1 otherwise.
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlist
+	dlistint_t *current, *temp;
+	unsigned int i;
+
+	if (*head == NULL)
+	return (-1);
+
+	current = *head;
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current->next == NULL)
+		return (-1);
+	current = current->next;
+	}
+	temp = current->next;
+
+	if (temp == NULL)
+		return (-1);
+	current->next = temp->next;
+	if (current->next != NULL)
+	current->next->prev = current;
+	free(temp);
+
+	return (1);
 }
