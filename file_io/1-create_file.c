6569d1194ed5aaed43243b5f9f62b4977f063c03
issaevis <evis.topollaj@gmail.com>
2023-04-11 08:42:05 -0700

    Added 1 and the extra files it needs to run. There may be an error down the line, will only know after the check

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file- Creates a textfile
+ *
+ * @filename: the name of the file we print from
+ * @text_content: the text we need to add to the file
+ *
+ * Return: Returns 1 if it works and 0 otherwise
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, f_write;
+
+	if (filename == NULL)
+		return (-1);
+
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	f_write = write(fd, text_content, strlen(text_content));
+	if (f_write == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+	return (1);
+}
