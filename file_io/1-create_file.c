4b4f688d06b70561c25ecb77124e7b22ae8f8eae
issaevis <evis.topollaj@gmail.com>
2023-04-11 08:45:49 -0700

    I may have fixed a bug in exercise 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,6 +26,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
 	f_write = write(fd, text_content, strlen(text_content));
 	if (f_write == -1)
 	{
