896af825df3608707f1816bff4564c2d46b7b250
issaevis <evis.topollaj@gmail.com>
2023-04-12 01:58:45 -0700

    Added the second exercise and the extra files it needs to work

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 /**
- * create_file- Creates a textfile
+ * append_text_to_file- Appends text to a textfile
  *
  * @filename: the name of the file we print from
  * @text_content: the text we need to add to the file
@@ -14,15 +14,15 @@
  * Return: Returns 1 if it works and -1 otherwise
 */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, f_write;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_APPEND | O_RDWR);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -33,11 +33,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	f_write = write(fd, text_content, strlen(text_content));
+
 	if (f_write == -1)
-	{
-		close(fd);
-		return (0);
-	}
+		return (-1);
 
 	close(fd);
 	return (1);
