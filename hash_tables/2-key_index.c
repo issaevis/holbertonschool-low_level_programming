04214a0e6a285dc14ffedb66ca939bb614b99efd
issaevis <evis.topollaj@gmail.com>
2023-04-13 07:07:10 -0700

    Fixed a simple Betty documentation error

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -4,7 +4,7 @@
 #include "hash_tables.h"
 
 /**
- * dey_index- a function that gives you the index of a key
+ * key_index- a function that gives you the index of a key
  *
  * @key: its the key of the node
  * @size: the size of the array of the hash table
